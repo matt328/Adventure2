af50074e52c1981e3b0b8b49c4104aa6ad5eb067
Matt <matt328@gmail.com>
2021-12-27 14:08:20 -0500

    Organize source folders

--- Adventure2/Source/pch.h
+++ Adventure2/Source/pch.h
@@ -35,16 +35,16 @@
 #include <wrl/event.h>
 
 #ifdef USING_DIRECTX_HEADERS
-#include <directx/dxgiformat.h>
 #include <directx/d3d12.h>
+#include <directx/dxgiformat.h>
 #else
 #include <d3d12.h>
 #endif
 
 #include <dxgi1_6.h>
 
-#include <DirectXMath.h>
 #include <DirectXColors.h>
+#include <DirectXMath.h>
 
 #include "d3dx12.h"
 
@@ -93,31 +93,24 @@
 #include "VertexTypes.h"
 #include "WICTextureLoader.h"
 
-namespace DX
-{
-	// Helper class for COM exceptions
-	class com_exception : public std::exception
-	{
-	public:
-		com_exception(HRESULT hr) noexcept : result(hr) {}
+namespace DX {
+   // Helper class for COM exceptions
+   class com_exception : public std::exception {
+    public:
+      com_exception(HRESULT hr) noexcept : result(hr) {}
 
-		const char* what() const override
-		{
-			static char s_str[64] = {};
-			sprintf_s(s_str, "Failure with HRESULT of %08X", static_cast<unsigned int>(result));
-			return s_str;
-		}
+      const char* what() const override {
+         static char s_str[64] = {};
+         sprintf_s(s_str, "Failure with HRESULT of %08X", static_cast<unsigned int>(result));
+         return s_str;
+      }
 
-	private:
-		HRESULT result;
-	};
+    private:
+      HRESULT result;
+   };
 
-	// Helper utility converts D3D API failures into exceptions.
-	inline void ThrowIfFailed(HRESULT hr)
-	{
-		if (FAILED(hr))
-		{
-			throw com_exception(hr);
-		}
-	}
-}
+   // Helper utility converts D3D API failures into exceptions.
+   inline void ThrowIfFailed(HRESULT hr) {
+      if (FAILED(hr)) { throw com_exception(hr); }
+   }
+} // namespace DX
